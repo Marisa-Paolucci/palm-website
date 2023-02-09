1357692846ec87a9ebaff494501a29b9f1ab1b76
Marisa Paolucci <marisapaolucciart@gmail.com>
2023-02-09 17:11:00 -0500

    added images and script.js. Updated files

---
+++ script.js
@@ -0,0 +1,8 @@
+const mobileMenuTrigger = document.getElementById('mobileMenuTrigger')
+const mobileMenu = document.querySelector('.nav-links-container');
+
+mobileMenuTrigger.addEventListener('click', ()=>
+{
+   mobileMenu.classList.toggle('active');
+   mobileMenuTrigger.classList.toggle('active'); 
+});
