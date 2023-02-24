2a43a7c05e759834e7adf5addab123030563f3d3
Marisa Paolucci <marisapaolucciart@gmail.com>
2023-02-24 09:56:48 -0500

    deleted palmbanner and palmlogo images, made accessibility updates to header and body in html, CSS, and JS.

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ mobileMenuTrigger.addEventListener('click', ()=>
    mobileMenu.classList.toggle('active');
    mobileMenuTrigger.classList.toggle('active'); 
 });
+
+var skipLink = document.querySelector('.skip-link');
+skipLink.addEventListener('click', function (e) {
+   document.querySelector(skipLink.getAttribute('href')).focus();
+});
