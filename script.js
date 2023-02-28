8d814f5daf31e87110e355a6bd9dce2b7fe4ddee
Marisa Paolucci <marisapaolucciart@gmail.com>
2023-02-28 15:44:52 -0500

    reduced images sizes, reformatted html and updated CSS

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const mobileMenuTrigger = document.getElementById('mobileMenuTrigger')
 const mobileMenu = document.querySelector('.nav-links-container');
 
-mobileMenuTrigger.addEventListener('click', ()=>
+mobileMenuTrigger.addEventListener('click', () =>
 {
    mobileMenu.classList.toggle('active');
    mobileMenuTrigger.classList.toggle('active'); 
+
+   //Toggle Aria-Expanded Attribute True / False
+   if(mobileMenu.classList.contains('active')) {
+      mobileMenuTrigger.setAttribute('aria-expanded', 'true');
+   } else {
+      mobileMenuTrigger.setAttribute('aria-expanded', 'false');
+   }
 });
 
 var skipLink = document.querySelector('.skip-link');
