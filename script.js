ac36dde6f23456eb2a15ad013a0a16e1f8580dcc
Marisa Paolucci <marisapaolucciart@gmail.com>
2023-03-02 15:20:56 -0500

    added images and made contact form with js error messages

--- script.js
+++ script.js
@@ -18,3 +18,39 @@ var skipLink = document.querySelector('.skip-link');
 skipLink.addEventListener('click', function (e) {
    document.querySelector(skipLink.getAttribute('href')).focus();
 });
+
+function showError(errorElement, errorMessage){
+   document.querySelector("."+errorElement).classList.add("display-error");
+   document.querySelector("."+errorElement).innerHTML = errorMessage;
+}
+
+function clearError(){
+   let errors = document.querySelectorAll(".error");
+   for(let error of errors){
+      error.classList.remove("display-error");
+   }
+}
+
+let form = document.forms['contact-us'];
+form.onsubmit = function(event){
+
+   clearError();
+
+   if(form.name.value === ""){
+      showError("name-error", "Please enter your name");
+      return false;
+   }
+
+   if(form.email.value === ""){
+      showError("email-error", "Please enter your email");
+      return false;
+   }
+
+   if(form.message.value === ""){
+      showError("message-error", "Please enter a message");
+      return false;
+   }
+
+
+   event.preventDefault();
+}
